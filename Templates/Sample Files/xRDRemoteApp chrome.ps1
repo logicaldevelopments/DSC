1e7ffecc154bf5e51e55de27a163524b3f9c9a80
logicaldevelopments <itservices@logdev.com>
2021-03-19 17:57:07 -0700

    Removing this repository

--- Templates/Sample Files/xRDRemoteApp chrome.ps1
+++
@@ -1,9 +0,0 @@
-xRDRemoteApp chrome {
- 
-CollectionName = $RDData.CollectionName
-Alias = 'Chrome'
-DisplayName = 'Chrome'
-FilePath = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-DependsOn = '[xRDLicenseConfiguration]licenseconfig'
-PsDscRunAsCredential = $domainadmin
-}
